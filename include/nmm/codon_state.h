014286f1af37960923a6232cd4bed219f90e0b72
Danilo Horta <danilo.horta@pm.me>
2020-05-07 17:20:06 +0100

    codon_state clean-up

--- include/nmm/codon_state.h
+++ include/nmm/codon_state.h
@@ -7,10 +7,10 @@ struct imm_state;
 struct nmm_codon_lprob;
 struct nmm_codon_state;
 
-NMM_API struct nmm_codon_state const* nmm_codon_state_create(
-    char const* name, struct nmm_codon_lprob const* codonp);
+NMM_API struct nmm_codon_state const* nmm_codon_state_create(char const*                   name,
+                                                             struct nmm_codon_lprob const* codonp);
 NMM_API struct nmm_codon_state const* nmm_codon_state_derived(struct imm_state const* state);
-NMM_API void                    nmm_codon_state_destroy(struct nmm_codon_state const* state);
-NMM_API struct imm_state const* nmm_codon_state_super(struct nmm_codon_state const* state);
+NMM_API void                          nmm_codon_state_destroy(struct nmm_codon_state const* state);
+NMM_API struct imm_state const*       nmm_codon_state_super(struct nmm_codon_state const* state);
 
 #endif
