c7241c037f99d38d4c67139d136fa6c239e16e0c
Danilo Horta <danilo.horta@pm.me>
2020-05-07 15:39:05 +0100

    codon_state clean-up

--- src/codon_state.c
+++ src/codon_state.c
@@ -17,28 +17,35 @@ struct nmm_codon_state
     struct nmm_base_abc const*    base_abc;
 };
 
-static uint8_t codon_state_type_id(struct imm_state const* state);
-static double  codon_state_lprob(struct imm_state const* state, struct imm_seq const* seq);
-static uint8_t codon_state_min_seq(struct imm_state const* state);
-static uint8_t codon_state_max_seq(struct imm_state const* state);
-static int  codon_state_write(struct imm_state const* state, struct imm_io const* io, FILE* stream);
-static void destroy(struct imm_state const* state);
+static void    destroy(struct imm_state const* state);
+static double  lprob(struct imm_state const* state, struct imm_seq const* seq);
+static uint8_t max_seq(struct imm_state const* state);
+static uint8_t min_seq(struct imm_state const* state);
+static uint8_t type_id(struct imm_state const* state);
+static int     write(struct imm_state const* state, struct imm_io const* io, FILE* stream);
 
-static struct imm_state_vtable const vtable = {codon_state_type_id, codon_state_lprob,
-                                               codon_state_min_seq, codon_state_max_seq,
-                                               codon_state_write,   destroy};
+static struct imm_state_vtable const __vtable = {destroy, lprob, max_seq, min_seq, type_id, write};
 
 struct nmm_codon_state const* nmm_codon_state_create(char const*                   name,
                                                      struct nmm_codon_lprob const* codonp)
 {
-    struct nmm_codon_state* state = malloc(sizeof(struct nmm_codon_state));
+    struct nmm_codon_state* state = malloc(sizeof(*state));
 
     state->codonp = codonp;
     state->base_abc = nmm_codon_lprob_abc(codonp);
 
     struct imm_abc const* abc = nmm_base_abc_super(nmm_codon_lprob_abc(codonp));
-    state->super = imm_state_create(name, abc, vtable, state);
+    state->super = imm_state_create(name, abc, __vtable, state);
     return state;
+}
+
+struct nmm_codon_state const* nmm_codon_state_derived(struct imm_state const* state)
+{
+    if (imm_state_type_id(state) != NMM_CODON_STATE_TYPE_ID) {
+        imm_error("could not cast to codon_state");
+        return NULL;
+    }
+    return __imm_state_derived(state);
 }
 
 void nmm_codon_state_destroy(struct nmm_codon_state const* state)
@@ -51,41 +58,10 @@ struct imm_state const* nmm_codon_state_super(struct nmm_codon_state const* stat
     return state->super;
 }
 
-struct nmm_codon_state const* nmm_codon_state_derived(struct imm_state const* state)
+struct nmm_codon_lprob const* codon_state_codonp(struct nmm_codon_state const* state)
 {
-    if (imm_state_type_id(state) != NMM_CODON_STATE_TYPE_ID) {
-        imm_error("could not cast to codon_state");
-        return NULL;
-    }
-    return __imm_state_derived(state);
+    return state->codonp;
 }
-
-static uint8_t codon_state_type_id(struct imm_state const* state)
-{
-    return NMM_CODON_STATE_TYPE_ID;
-}
-
-static double codon_state_lprob(struct imm_state const* state, struct imm_seq const* seq)
-{
-    struct nmm_codon_state const* s = __imm_state_derived(state);
-    unsigned                      length = imm_seq_length(seq);
-
-    if (length != 3)
-        return imm_lprob_invalid();
-
-    NMM_CODON_DECL(codon, s->base_abc);
-
-    struct nmm_triplet const triplet = NMM_TRIPLET(seq->string[0], seq->string[1], seq->string[2]);
-
-    if (nmm_codon_set_triplet(&codon, triplet))
-        return imm_lprob_invalid();
-
-    return nmm_codon_lprob_get(s->codonp, &codon);
-}
-
-static uint8_t codon_state_min_seq(struct imm_state const* state) { return 3; }
-
-static uint8_t codon_state_max_seq(struct imm_state const* state) { return 3; }
 
 struct imm_state const* codon_state_read(FILE* stream, struct nmm_io const* io)
 {
@@ -96,10 +72,9 @@ struct imm_state const* codon_state_read(FILE* stream, struct nmm_io const* io)
         return NULL;
     }
 
-    state->vtable = vtable;
-
     struct nmm_codon_state* codon_state = malloc(sizeof(*codon_state));
     codon_state->super = state;
+    state->vtable = __vtable;
     state->derived = codon_state;
 
     if (!(codon_state->base_abc = nmm_base_abc_derived(abc))) {
@@ -128,7 +103,37 @@ err:
     return NULL;
 }
 
-static int codon_state_write(struct imm_state const* state, struct imm_io const* io, FILE* stream)
+static void destroy(struct imm_state const* state)
+{
+    free_c(__imm_state_derived(state));
+    __imm_state_destroy(state);
+}
+
+static double lprob(struct imm_state const* state, struct imm_seq const* seq)
+{
+    struct nmm_codon_state const* s = __imm_state_derived(state);
+    unsigned                      length = imm_seq_length(seq);
+
+    if (length != 3)
+        return imm_lprob_invalid();
+
+    NMM_CODON_DECL(codon, s->base_abc);
+
+    struct nmm_triplet const triplet = NMM_TRIPLET(seq->string[0], seq->string[1], seq->string[2]);
+
+    if (nmm_codon_set_triplet(&codon, triplet))
+        return imm_lprob_invalid();
+
+    return nmm_codon_lprob_get(s->codonp, &codon);
+}
+
+static uint8_t max_seq(struct imm_state const* state) { return 3; }
+
+static uint8_t min_seq(struct imm_state const* state) { return 3; }
+
+static uint8_t type_id(struct imm_state const* state) { return NMM_CODON_STATE_TYPE_ID; }
+
+static int write(struct imm_state const* state, struct imm_io const* io, FILE* stream)
 {
     struct nmm_codon_state const* s = nmm_codon_state_derived(state);
     uint32_t                      index = io_codonp_index(nmm_io_derived(io), s->codonp);
@@ -144,16 +149,4 @@ static int codon_state_write(struct imm_state const* state, struct imm_io const*
     }
 
     return 0;
-}
-
-static void destroy(struct imm_state const* state)
-{
-    struct nmm_codon_state const* s = __imm_state_derived(state);
-    free_c(s);
-    __imm_state_destroy(state);
-}
-
-struct nmm_codon_lprob const* codon_state_codonp(struct nmm_codon_state const* state)
-{
-    return state->codonp;
 }
